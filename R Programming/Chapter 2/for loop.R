0d26ea940267be3001f6556b53e37e98ee6f7577
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-26 13:36:14 +0300

    Update for loop.R

--- R Programming/Chapter 2/for loop.R
+++ R Programming/Chapter 2/for loop.R
@@ -16,3 +16,22 @@ for( city in cities) {
 "Tokyo"
 "Rio de Janeiro"
 "Capetown"
+
+##################################################
+"break statements within for loops."
+"stops the execution of for loop and abandons the execution all together."
+
+cities <- list("New York", "Paris", "London", "Tokyo", "Rio de Janeiro", "Capetown")
+
+for( city in cities) {
+    if (nchar(city) == 6) {
+      break
+    }
+  print(city)
+
+  ##############  OUTPUT  ##############
+  
+"New York"
+"Paris"
+
+"next statement also alters the flow of your for loop"
