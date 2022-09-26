521223f40f10a926e2549792f2b0589ec2bf7ff3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-26 13:40:31 +0300

    Update for loop.R

--- R Programming/Chapter 2/for loop.R
+++ R Programming/Chapter 2/for loop.R
@@ -36,12 +36,20 @@ for( city in cities) {
   
 ##################################################
 "next statement also alters the flow of your for loop"
+ "All city names except London gets printed to the console coz,the next statement skips the remainder of the code inside the for loop and proceeds to the next acturation."
   
 cities <- list("New York", "Paris", "London", "Tokyo", "Rio de Janeiro", "Capetown")
 
 for( city in cities) {
     if (nchar(city) == 6) {
-      break
+      next
     }
   print(city)
 
+################ OUTPUT #######################
+  
+"New York"
+"Paris"
+"Tokyo"
+"Rio de Janeiro"
+"Capetown"
