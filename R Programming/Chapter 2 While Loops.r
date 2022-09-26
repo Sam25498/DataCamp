527a12ad187bc3c9cd4760113a7c0cd0837f56ab
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-26 12:56:53 +0300

    Update Chapter 2 While Loops.r

--- R Programming/Chapter 2 While Loops.r
+++ R Programming/Chapter 2 While Loops.r
@@ -7,3 +7,27 @@ while(ctr <= 7) {
   print(paste("ctr is set to", ctr))
   ctr <- ctr + 1
 }
+###########################################
+#Exercise
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  print("Slow down!")
+  speed <- speed - 7
+}
+
+# Print out the speed variable
+speed 
+
+############     OUTPUT   ################
+
+[1] "Slow down!"
+[1] "Slow down!"
+[1] "Slow down!"
+[1] "Slow down!"
+[1] "Slow down!"
+# Print out the speed variable
+speed
+[1] 29
