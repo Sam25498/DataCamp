3f2ad776316bec8098cc4ac71a38180980ca1722
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-27 13:28:02 +0300

    Update Loop over a vector.R

--- R Programming/Chapter 2/Loop over a vector.R
+++ R Programming/Chapter 2/Loop over a vector.R
@@ -1,3 +1,4 @@
+#######################    Exercise 1     #####################
 # The linkedin vector has already been defined for you
 linkedin <- c(16, 9, 13, 5, 2, 17, 14)
 
@@ -11,3 +12,27 @@ for (v in linkedin) {
 for (i in 1:length(linkedin)) {
     print(linkedin[i])
 }
+
+####################     OUTPUT     #########################
+# Loop version 1
+for (v in linkedin) {
+    print(v)
+}
+[1] 16
+[1] 9
+[1] 13
+[1] 5
+[1] 2
+[1] 17
+[1] 14
+# Loop version 2
+for (i in 1:length(linkedin)) {
+    print(linkedin[i])
+}
+[1] 16
+[1] 9
+[1] 13
+[1] 5
+[1] 2
+[1] 17
+[1] 14
