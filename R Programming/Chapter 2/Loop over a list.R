ca050932f31fe3e60edf6c7138fed559f306f904
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-27 13:37:36 +0300

    Update Loop over a list.R

--- R Programming/Chapter 2/Loop over a list.R
+++ R Programming/Chapter 2/Loop over a list.R
@@ -15,3 +15,22 @@ for (i in nyc) {
 for (t in 1:length(nyc)) {
     print(nyc[[t]])
 }
+
+##########################     OUTPUT     ##########################
+
+# Loop version 1
+for (i in nyc) {
+    print(i)
+}
+[1] 8405837
+[1] "Manhattan"     "Bronx"         "Brooklyn"      "Queens"       
+[5] "Staten Island"
+[1] FALSE
+# Loop version 2
+for (t in 1:length(nyc)) {
+    print(nyc[[t]])
+}
+[1] 8405837
+[1] "Manhattan"     "Bronx"         "Brooklyn"      "Queens"       
+[5] "Staten Island"
+[1] FALSE
