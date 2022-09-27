16b8b5adbf89fb374d6fd300b0356b48b421fc0b
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-27 13:34:51 +0300

    Create Loop over a list.R

---
+++ R Programming/Chapter 2/Loop over a list.R
@@ -0,0 +1,17 @@
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for (i in nyc) {
+    print(i)
+}
+
+
+
+
+# Loop version 2
+for (t in 1:length(nyc)) {
+    print(nyc[[t]])
+}
