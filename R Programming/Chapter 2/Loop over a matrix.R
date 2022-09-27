e2e47e7567794aa6b18f4f762e069945a96079d0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-09-27 13:50:46 +0300

    Update Loop over a matrix.R

--- R Programming/Chapter 2/Loop over a matrix.R
+++ R Programming/Chapter 2/Loop over a matrix.R
@@ -5,17 +5,18 @@
 # define the double for loop
 for (i in 1:nrow(ttt)) {
   for (j in 1:ncol(ttt)) {
-    print(paste( "On row i and column j the board contains x"))
+    print(paste( "On row", i ,"and column", j ,"the board contains", ttt[i,j]))
   }
 }
 
 ######################   OUTPUT  ######################
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
-[1] "On row i and column j the board contains x"
+
+[1] "On row 1 and column 1 the board contains O"
+[1] "On row 1 and column 2 the board contains NA"
+[1] "On row 1 and column 3 the board contains X"
+[1] "On row 2 and column 1 the board contains NA"
+[1] "On row 2 and column 2 the board contains O"
+[1] "On row 2 and column 3 the board contains O"
+[1] "On row 3 and column 1 the board contains X"
+[1] "On row 3 and column 2 the board contains NA"
+[1] "On row 3 and column 3 the board contains X"
