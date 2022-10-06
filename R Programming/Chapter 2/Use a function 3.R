f342668ead0b61660f2917e3ac173d0836e292ac
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-06 14:04:20 +0300

    Create Use a function 3.R

---
+++ R Programming/Chapter 2/Use a function 3.R
@@ -0,0 +1,11 @@
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, NA, 17, 14)
+facebook <- c(17, NA, 5, 16, 8, 13, 14)
+
+# Basic average of linkedin
+avg_li <- mean(linkedin)
+avg_li
+
+# Advanced average of linkedin
+avg_li <- mean(linkedin, na.rm = TRUE)
+avg_li
