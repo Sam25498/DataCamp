100b91964b494217d34afae1f528348d5f857859
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-12 14:30:25 +0300

    Create Different ways to load a package.R

---
+++ R Programming/Chapter3/Different ways to load a package.R
@@ -0,0 +1,14 @@
+# Chunk 1
+library(data.table)
+require(rjson)
+
+# Chunk 2
+library("data.table")
+require(rjson)
+
+# Chunk 3
+library(data.table)
+require(rjson, character.only = TRUE)
+
+# Chunk 4
+library(c("data.table", "rjson"))
