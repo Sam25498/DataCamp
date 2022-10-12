3ea6d9d2fd689d88d4fae0b392853fee872ccaa4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-12 14:26:33 +0300

    Update Load an R Package.R

--- R Programming/Chapter3/Load an R Package.R
+++ R Programming/Chapter3/Load an R Package.R
@@ -6,3 +6,9 @@ qplot(mtcars$wt, mtcars$hp)
 
 # Check out the currently attached packages again
 search()
+
+#Output 
+ [1] ".GlobalEnv"        "package:ggplot2"   "package:RBackend" 
+ [4] "package:stats"     "package:graphics"  "package:grDevices"
+ [7] "package:utils"     "package:datasets"  "package:methods"  
+[10] "Autoloads"         "package:base"     
