90425921d26bd89841bffd2bb2800c3b0da0fcc3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-13 15:00:43 +0300

    Update sapply with functions that return NULL.R

--- R Programming/Chapter 4/sapply with functions that return NULL.R
+++ R Programming/Chapter 4/sapply with functions that return NULL.R
@@ -12,3 +12,69 @@ info_s
 # Apply print_info() over temp using lapply()
 info_l <- lapply(temp, print_info)
 info_l
+
+################################################################################################
+####################                        OUTPUT                        ######################
+################################################################################################
+
+# Apply print_info() over temp using sapply()
+info_s <- sapply(temp, print_info)
+The average temperature is 4.8 
+The average temperature is 9 
+The average temperature is 2.2 
+The average temperature is 2.4 
+The average temperature is 5.4 
+The average temperature is 4.6 
+The average temperature is 4.6 
+info_s
+[[1]]
+NULL
+
+[[2]]
+NULL
+
+[[3]]
+NULL
+
+[[4]]
+NULL
+
+[[5]]
+NULL
+
+[[6]]
+NULL
+
+[[7]]
+NULL
+# Apply print_info() over temp using lapply()
+info_l <- lapply(temp, print_info)
+The average temperature is 4.8 
+The average temperature is 9 
+The average temperature is 2.2 
+The average temperature is 2.4 
+The average temperature is 5.4 
+The average temperature is 4.6 
+The average temperature is 4.6 
+info_l
+[[1]]
+NULL
+
+[[2]]
+NULL
+
+[[3]]
+NULL
+
+[[4]]
+NULL
+
+[[5]]
+NULL
+
+[[6]]
+NULL
+
+[[7]]
+NULL
+  
