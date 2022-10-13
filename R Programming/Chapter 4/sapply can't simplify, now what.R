51687cd2b2b0b799457b303194e30d6151071dd2
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-13 14:55:58 +0300

    Update sapply can't simplify, now what.R

--- R Programming/Chapter 4/sapply can't simplify, now what.R
+++ R Programming/Chapter 4/sapply can't simplify, now what.R
@@ -15,3 +15,57 @@ freezing_l
 
 # Are freezing_s and freezing_l identical?
 identical(freezing_s,freezing_l)
+
+########################################################################################################
+################################            OUTPUT             #########################################
+########################################################################################################
+
+# Apply below_zero over temp using sapply(): freezing_s
+freezing_s <- sapply(temp, below_zero)
+freezing_s
+[[1]]
+[1] -1
+
+[[2]]
+numeric(0)
+
+[[3]]
+[1] -1 -3
+
+[[4]]
+[1] -2
+
+[[5]]
+numeric(0)
+
+[[6]]
+[1] -3
+
+[[7]]
+numeric(0)
+# Apply below_zero over temp using lapply(): freezing_l
+freezing_l <- lapply(temp, below_zero)
+freezing_l
+[[1]]
+[1] -1
+
+[[2]]
+numeric(0)
+
+[[3]]
+[1] -1 -3
+
+[[4]]
+[1] -2
+
+[[5]]
+numeric(0)
+
+[[6]]
+[1] -3
+
+[[7]]
+numeric(0)
+# Are freezing_s and freezing_l identical?
+identical(freezing_s,freezing_l)
+[1] TRUE
