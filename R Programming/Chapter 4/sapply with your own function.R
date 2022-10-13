573da2fc02838bdf23d4ef5704b0f4dd04539e87
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-13 14:42:24 +0300

    Update sapply with your own function.R

--- R Programming/Chapter 4/sapply with your own function.R
+++ R Programming/Chapter 4/sapply with your own function.R
@@ -12,3 +12,37 @@ extrm_avg_temp
 # Apply extremes_avg() over temp using lapply()
 avg_temp_extrm <- lapply(temp, extremes_avg)
 avg_temp_extrm
+
+
+
+#######################################################################################################
+##################################         OUTPUT           ###########################################
+#######################################################################################################
+
+# Apply extremes_avg() over temp using sapply()
+extrm_avg_temp <- sapply(temp, extremes_avg)
+extrm_avg_temp
+[1] 4.0 9.0 2.5 2.5 5.5 3.0 5.0
+# Apply extremes_avg() over temp using lapply()
+avg_temp_extrm <- lapply(temp, extremes_avg)
+avg_temp_extrm
+[[1]]
+[1] 4
+
+[[2]]
+[1] 9
+
+[[3]]
+[1] 2.5
+
+[[4]]
+[1] 2.5
+
+[[5]]
+[1] 5.5
+
+[[6]]
+[1] 3
+
+[[7]]
+[1] 5
