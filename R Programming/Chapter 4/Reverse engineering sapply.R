86ba75ee3e5dba651e1cad975da63e37c339ae1d
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-14 14:53:08 +0300

    Update Reverse engineering sapply.R

--- R Programming/Chapter 4/Reverse engineering sapply.R
+++ R Programming/Chapter 4/Reverse engineering sapply.R
@@ -1,3 +1,9 @@
 
 sapply(list(runif (10), runif (10)), 
        function(x) c(min = min(x), mean = mean(x), max = max(x)))
+  #OUTPUT     
+                 [,1]         [,2]
+min  0.1137891 0.0008211893
+mean 0.7075331 0.5088828115
+max  0.9404418 0.9515799196
+
