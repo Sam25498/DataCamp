83e533a9c2ac61a057be3133afdb52c54d3daa6d
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-14 15:13:48 +0300

    Update use vapply 2.R

--- R Programming/Chapter 4/use vapply 2.R
+++ R Programming/Chapter 4/use vapply 2.R
@@ -7,3 +7,11 @@ basics <- function(x) {
 
 # Fix the error:
 vapply(temp, basics, numeric(4))
+
+#OUTPUT
+vapply(temp, basics, numeric(4))
+       [,1] [,2] [,3] [,4] [,5] [,6] [,7]
+min    -1.0    5 -3.0 -2.0  2.0 -3.0  1.0
+mean    4.8    9  2.2  2.4  5.4  4.6  4.6
+median  6.0    9  3.0  2.0  5.0  5.0  4.0
+max     9.0   13  8.0  7.0  9.0  9.0  9.0
