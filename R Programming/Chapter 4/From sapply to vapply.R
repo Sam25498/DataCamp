53bc96c4f8acb30cd17204b6506fb0c96c420511
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-14 15:18:57 +0300

    Update From sapply to vapply.R

--- R Programming/Chapter 4/From sapply to vapply.R
+++ R Programming/Chapter 4/From sapply to vapply.R
@@ -9,8 +9,9 @@ vapply(temp, function(x, y) { mean(x) > y }, y = 5, logical(1))
 ##################################################
 # OUTPUT
  # Convert to vapply() expression
-vapply(temp, max, numeric(1))
-[1]  9 13  8  7  9  9  9
+#vapply(temp, max, numeric(1))
+#[1]  9 13  8  7  9  9  9
+
 # Convert to vapply() expression
-vapply(temp, function(x, y) { mean(x) > y }, y = 5, logical(1))
-[1] FALSE  TRUE FALSE FALSE  TRUE FALSE FALSE
+#vapply(temp, function(x, y) { mean(x) > y }, y = 5, logical(1))
+#[1] FALSE  TRUE FALSE FALSE  TRUE FALSE FALSE
