c94f568d0f072846ddf82d07d791bc9b8de02657
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-14 15:11:08 +0300

    Update Use Vapply.R

--- R Programming/Chapter 4/Use Vapply.R
+++ R Programming/Chapter 4/Use Vapply.R
@@ -8,3 +8,38 @@ basics <- function(x) {
 # Apply basics() over temp using vapply()
 t <- vapply(temp, basics, numeric(3))
 t
+
+#output
+# temp is already available in the workspace
+temp
+[[1]]
+[1]  3  7  9  6 -1
+
+[[2]]
+[1]  6  9 12 13  5
+
+[[3]]
+[1]  4  8  3 -1 -3
+
+[[4]]
+[1]  1  4  7  2 -2
+
+[[5]]
+[1] 5 7 9 4 2
+
+[[6]]
+[1] -3  5  8  9  4
+
+[[7]]
+[1] 3 6 9 4 1
+# Definition of basics()
+basics <- function(x) {
+  c(min = min(x), mean = mean(x), max = max(x))
+}
+# Apply basics() over temp using vapply()
+t <- vapply(temp, basics, numeric(3))
+t
+     [,1] [,2] [,3] [,4] [,5] [,6] [,7]
+min  -1.0    5 -3.0 -2.0  2.0 -3.0  1.0
+mean  4.8    9  2.2  2.4  5.4  4.6  4.6
+max   9.0   13  8.0  7.0  9.0  9.0  9.0
