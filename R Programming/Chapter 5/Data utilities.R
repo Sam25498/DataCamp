d9d2f1e101b33d02032559e37451675462018827
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-15 22:53:56 +0300

    Update Data utilities.R

--- R Programming/Chapter 5/Data utilities.R
+++ R Programming/Chapter 5/Data utilities.R
@@ -12,3 +12,12 @@ social_vec
 
 # Sort social_vec
 sort(social_vec, decreasing = TRUE)
+
+#OUTPUT
+# Append fb_vec to li_vec: social_vec
+social_vec <- append(li_vec, fb_vec)
+social_vec
+ [1] 16  9 13  5  2 17 14 17  7  5 16  8 13 14
+# Sort social_vec
+sort(social_vec, decreasing = TRUE)
+ [1] 17 17 16 16 14 14 13 13  9  8  7  5  5  2
