831dc1179630d236a695a8f87ba45ea64a6c4f2d
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 22:53:09 +0300

    Update regex sub & gsub.R

--- R Programming/Chapter 5/regex sub & gsub.R
+++ R Programming/Chapter 5/regex sub & gsub.R
@@ -2,3 +2,8 @@
 emails <- c("john.doe@ivyleague.edu", "education@world.gov", "global@peace.org",
             "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
             
+# Use sub() to convert the email domains to datacamp.edu
+sub(pattern = "@.*\\.edu$", replacement = "@datacamp.edu", x = emails)
+[1] "john.doe@datacamp.edu"    "education@world.gov"     
+[3] "global@peace.org"         "invalid.edu"             
+[5] "quant@datacamp.edu"       "cookie.monster@sesame.tv"
