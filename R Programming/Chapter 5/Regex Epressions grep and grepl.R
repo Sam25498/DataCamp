29f368390eb78d237044c7bd6ce459921ac4adc3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 22:38:11 +0300

    Update Regex Epressions grep and grepl.R

--- R Programming/Chapter 5/Regex Epressions grep and grepl.R
+++ R Programming/Chapter 5/Regex Epressions grep and grepl.R
@@ -13,3 +13,25 @@ hits
 hits[1:4]
 emails[hits[1]]
 emails[hits[1:4]]
+
+
+#OUTPUT
+
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org",
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+# Use grepl() to match for "edu"
+grepl(pattern = "edu", x = emails)
+[1]  TRUE  TRUE FALSE  TRUE  TRUE FALSE
+# Use grep() to match for "edu", save result to hits
+hits <- grep(pattern = "edu", x = emails)
+hits
+[1] 1 2 4 5
+# Subset emails using hits
+hits[1:4]
+[1] 1 2 4 5
+emails[hits[1]]
+[1] "john.doe@ivyleague.edu"
+emails[hits[1:4]]
+[1] "john.doe@ivyleague.edu"   "education@world.gov"     
+[3] "invalid.edu"              "quant@bigdatacollege.edu"
