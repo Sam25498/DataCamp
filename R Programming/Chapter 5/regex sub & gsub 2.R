0ccb12528954e2e2e6fe91651d1b8e69b8e70c1b
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 23:00:43 +0300

    Update regex sub & gsub 2.R

--- R Programming/Chapter 5/regex sub & gsub 2.R
+++ R Programming/Chapter 5/regex sub & gsub 2.R
@@ -6,3 +6,6 @@ awards <- c("Won 1 Oscar.",
   "4 wins & 1 nomination.")
 
 sub(".*\\s([0-9]+)\\snomination.*$", "\\1", awards)
+#output: A vector of character strings containing "Won 1 Oscar.", "24", "2", "3", "2", "1".
+# because The ([0-9]+) selects the entire number that comes before the word “nomination” in the string, and the entire match gets replaced by this number because of the \\1 that reference to the content inside the parentheses. 
+#The next video will get you up to speed with times and dates in R!
