5870f4270e7a581abf95af7251ebedac6d18b72d
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 22:47:27 +0300

    Update Regex Epressions grep and grepl 2.R

--- R Programming/Chapter 5/Regex Epressions grep and grepl 2.R
+++ R Programming/Chapter 5/Regex Epressions grep and grepl 2.R
@@ -6,3 +6,11 @@ emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.o
 grepl(pattern = "@.*\\.edu$", x = emails)
 [1]  TRUE FALSE FALSE FALSE  TRUE FALSE
 
+# Use grep() to match for .edu addresses more robustly, save result to hits
+hits <- grep(pattern =  "@.*\\.edu$", x = emails)
+hits
+#[1] 1 5
+
+# Subset emails using hits
+emails[hits[1:2]]
+[1] "john.doe@ivyleague.edu"   "quant@bigdatacollege.edu"
