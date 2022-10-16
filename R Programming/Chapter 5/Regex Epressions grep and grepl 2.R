cd3636a75c2d4b84836b8d0b287e61eb84361203
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 22:47:13 +0300

    Create Regex Epressions grep and grepl 2.R

---
+++ R Programming/Chapter 5/Regex Epressions grep and grepl 2.R
@@ -0,0 +1,8 @@
+# The emails vector has already been defined for you
+
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org",
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+# Use grepl() to match for .edu addresses more robustly
+grepl(pattern = "@.*\\.edu$", x = emails)
+[1]  TRUE FALSE FALSE FALSE  TRUE FALSE
+
