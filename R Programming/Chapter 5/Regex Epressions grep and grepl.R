0e4e3390fe12ffebfd99ca5b2e0dab4e10030ef3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-16 22:36:58 +0300

    Create Regex Epressions grep and grepl.R

---
+++ R Programming/Chapter 5/Regex Epressions grep and grepl.R
@@ -0,0 +1,15 @@
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org",
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+
+# Use grepl() to match for "edu"
+grepl(pattern = "edu", x = emails)
+
+
+# Use grep() to match for "edu", save result to hits
+hits <- grep(pattern = "edu", x = emails)
+hits
+# Subset emails using hits
+hits[1:4]
+emails[hits[1]]
+emails[hits[1:4]]
