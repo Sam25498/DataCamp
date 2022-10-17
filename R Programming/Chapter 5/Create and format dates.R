bf6e9dfc706401bff2fed7732d5c8a3f4fd0a31e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-17 23:44:34 +0300

    Update Create and format dates.R

--- R Programming/Chapter 5/Create and format dates.R
+++ R Programming/Chapter 5/Create and format dates.R
@@ -14,3 +14,16 @@ date3
 format(date1, "%A")
 format(date2, "%d")
 format(date3, "%b %Y")
+
+##############################################################
+######################    OUTPUT    ##########################
+
+date3
+[1] "2006-01-30"
+# Convert dates to formatted strings
+format(date1, "%A")
+[1] "Thursday"
+format(date2, "%d")
+[1] "15"
+format(date3, "%b %Y")
+[1] "Jan 2006"
