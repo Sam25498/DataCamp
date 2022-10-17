00a84d5092e4923639ed0dd564a089efa3022cbb
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-17 23:28:23 +0300

    Update Right here, right now.R

--- R Programming/Chapter 5/Right here, right now.R
+++ R Programming/Chapter 5/Right here, right now.R
@@ -14,3 +14,18 @@ now
 
 # See what now looks like under the hood
 unclass(now)
+
+
+#OUTPUT
+# Get the current date: today
+today <- Sys.Date()
+today
+[1] "2022-10-17"
+# See what today looks like under the hood
+unclass(today)
+[1] 19282
+# Get the current time: now
+now <- Sys.time()
+now
+[1] "2022-10-17 20:26:19 UTC"
+
