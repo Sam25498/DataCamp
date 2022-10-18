9c6627b73826bc3e6fcbfab70538171a1c1f5e4b
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-18 22:25:03 +0300

    Update Calculations with Times.R

--- R Programming/Chapter 5/Calculations with Times.R
+++ R Programming/Chapter 5/Calculations with Times.R
@@ -11,3 +11,18 @@ sum(time_online)
 # Calculate the average time online
 avg_time_online <- mean(time_online )
 avg_time_online
+
+
+############################################################################
+#####################   OUTPUT    ##########################################
+# Inspect the variable time_online
+time_online
+Time differences in secs
+[1] 2305.11818   34.18472  837.18182 2397.90153 1851.30411
+# Calculate the total time online
+sum(time_online)
+Time difference of 7425.69 secs
+# Calculate the average time online
+avg_time_online <- mean(time_online )
+avg_time_online
+Time difference of 1485.138 secs
