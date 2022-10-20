1cb8811c7aaf35e064ab2766dfa7bf46992c2ac4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-20 21:19:21 +0300

    Create Combining filter, mutate and arrange.R

---
+++ Tidyverse/Chapter 1/Combining filter, mutate and arrange.R
@@ -0,0 +1,7 @@
+library(gapminder)
+library(dplyr)
+# Filter, mutate, and arrange the gapminder dataset
+gapminder %>%
+filter(year == 2007) %>%
+mutate(lifeExpMonths = 12 * lifeExp) %>%
+arrange(desc(lifeExpMonths))
