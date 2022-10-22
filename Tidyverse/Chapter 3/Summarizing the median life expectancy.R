8dfbed26a9c1dd4c6fe7960ec8bf680207137629
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-22 22:28:12 +0300

    Update Summarizing the median life expectancy.R

--- Tidyverse/Chapter 3/Summarizing the median life expectancy.R
+++ Tidyverse/Chapter 3/Summarizing the median life expectancy.R
@@ -4,3 +4,10 @@ library(dplyr)
 # Summarize to find the median life expectancy
 gapminder %>%  summarize(medianLifeExp = median(lifeExp))
 
+##############################################
+###########  OUTPUT   ########################
+
+# A tibble: 1 × 1
+  medianLifeExp
+          <dbl>
+1          60.7
