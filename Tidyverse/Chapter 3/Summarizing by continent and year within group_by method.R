aaa7e4ad67c6929a00b417176f636df3f66b0f17
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-23 14:21:07 +0300

    Update Summarizing by continent and year within group_by method.R

--- Tidyverse/Chapter 3/Summarizing by continent and year within group_by method.R
+++ Tidyverse/Chapter 3/Summarizing by continent and year within group_by method.R
@@ -3,3 +3,21 @@ library(dplyr)
 
 # Find median life expectancy and maximum GDP per capita in each continent/year combination
 gapminder %>% group_by(year,continent) %>% summarize(medianLifeExp =  median(lifeExp), maxGdpPercap = max(gdpPercap))
+
+
+####################################################################3
+# A tibble: 60 × 4
+# Groups:   year [12]
+    year continent medianLifeExp maxGdpPercap
+   <int> <fct>             <dbl>        <dbl>
+ 1  1952 Africa             38.8        4725.
+ 2  1952 Americas           54.7       13990.
+ 3  1952 Asia               44.9      108382.
+ 4  1952 Europe             65.9       14734.
+ 5  1952 Oceania            69.3       10557.
+ 6  1957 Africa             40.6        5487.
+ 7  1957 Americas           56.1       14847.
+ 8  1957 Asia               48.3      113523.
+ 9  1957 Europe             67.6       17909.
+10  1957 Oceania            70.3       12247.
+# … with 50 more rows
