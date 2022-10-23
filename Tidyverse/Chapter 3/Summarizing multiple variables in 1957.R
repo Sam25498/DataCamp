9c19fe0b89a9974cd63eb3532eedb6d626856177
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-23 13:58:59 +0300

    Update Summarizing multiple variables in 1957.R

--- Tidyverse/Chapter 3/Summarizing multiple variables in 1957.R
+++ Tidyverse/Chapter 3/Summarizing multiple variables in 1957.R
@@ -3,3 +3,10 @@ library(dplyr)
 
 # Filter for 1957 then summarize the median life expectancy and the maximum GDP per capita
 gapminder %>% filter( year == 1957) %>% summarize(medianLifeExp = median(lifeExp), maxGdpPercap= max(gdpPercap))
+
+############################################################################
+
+# A tibble: 1 × 2
+  medianLifeExp maxGdpPercap
+          <dbl>        <dbl>
+1          48.4      113523.
