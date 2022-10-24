7c2bf7a2eb6a698f2686391d0287991025839617
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-24 22:52:31 +0300

    Update Comparing median life expectancy and median GDP per continent in 2007.R

--- Tidyverse/Chapter 3/Comparing median life expectancy and median GDP per continent in 2007.R
+++ Tidyverse/Chapter 3/Comparing median life expectancy and median GDP per continent in 2007.R
@@ -6,6 +6,15 @@ library(ggplot2)
 by_continent_2007 <- gapminder %>% filter(year == 2007) %>% group_by(continent) %>% summarize(medianLifeExp = median(lifeExp), medianGdpPercap = median(gdpPercap))
 
 by_continent_2007
+
+# A tibble: 5 × 3
+  continent medianLifeExp medianGdpPercap
+  <fct>             <dbl>           <dbl>
+1 Africa             52.9           1452.
+2 Americas           72.9           8948.
+3 Asia               72.4           4471.
+4 Europe             78.6          28054.
+5 Oceania            80.7          29810.
 # Use a scatter plot to compare the median GDP and median life expectancy
 
 ggplot(by_continent_2007, aes(x = medianGdpPercap , y = medianLifeExp , color = continent)) + geom_point() + expand_limits(y = 0)
