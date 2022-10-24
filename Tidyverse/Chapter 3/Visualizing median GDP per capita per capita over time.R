f85385fbad9995147fe05c0bea41dab92d5335d1
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-24 23:04:25 +0300

    Update Visualizing median GDP per capita per capita over time.R

--- Tidyverse/Chapter 3/Visualizing median GDP per capita per capita over time.R
+++ Tidyverse/Chapter 3/Visualizing median GDP per capita per capita over time.R
@@ -6,5 +6,22 @@ library(ggplot2)
 by_year <- gapminder %>%  group_by(year) %>% summarize(medianGdpPercap = median(gdpPercap))
 
 by_year
+#################           OUTPUT             ######################
+# A tibble: 12 × 2
+    year medianGdpPercap
+   <int>           <dbl>
+ 1  1952           1969.
+ 2  1957           2173.
+ 3  1962           2335.
+ 4  1967           2678.
+ 5  1972           3339.
+ 6  1977           3799.
+ 7  1982           4216.
+ 8  1987           4280.
+ 9  1992           4386.
+10  1997           4782.
+11  2002           5320.
+12  2007           6124.
+###############################################################
 # Create a line plot showing the change in medianGdpPercap over time
 ggplot(by_year, aes(x = year , y = medianGdpPercap )) + geom_line() + expand_limits(y = 0)
