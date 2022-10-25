4fa7a265ee865c7a6dc5019d37c34bc879cb162e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-25 22:35:04 +0300

    Update Visualizing GDP per capita by country in Oceania.R

--- Tidyverse/Chapter 3/Bar Plot/Visualizing GDP per capita by country in Oceania.R
+++ Tidyverse/Chapter 3/Bar Plot/Visualizing GDP per capita by country in Oceania.R
@@ -8,3 +8,12 @@ oceania_1952
 
 # Create a bar plot of gdpPercap by country
 ggplot(oceania_1952, aes(x = country, y = gdpPercap)) + geom_col()
+
+
+########################################################################
+# oceania_1952 output
+# A tibble: 2 × 6
+  country     continent  year lifeExp     pop gdpPercap
+  <fct>       <fct>     <int>   <dbl>   <int>     <dbl>
+1 Australia   Oceania    1952    69.1 8691212    10040.
+2 New Zealand Oceania    1952    69.4 1994794    10557.
