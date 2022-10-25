1ec4d3a118bfaeacaf29834fcb21af65bd3cb9a6
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-25 22:34:12 +0300

    Create Visualizing GDP per capita by country in Oceania.R

---
+++ Tidyverse/Chapter 3/Bar Plot/Visualizing GDP per capita by country in Oceania.R
@@ -0,0 +1,10 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Filter for observations in the Oceania continent in 1952
+oceania_1952 <- gapminder %>% filter(year == 1952) %>% filter(continent == "Oceania")
+oceania_1952
+
+# Create a bar plot of gdpPercap by country
+ggplot(oceania_1952, aes(x = country, y = gdpPercap)) + geom_col()
