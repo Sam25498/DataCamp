2892f1d470e8349d5adf153fb23b30b27a6a53fa
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-28 22:50:30 +0300

    Update Type conversion.py

--- Python/Introduction to Python/Chapter 1/Type conversion.py
+++ Python/Introduction to Python/Chapter 1/Type conversion.py
@@ -4,7 +4,9 @@ result = 100 * 1.10 ** 7
 
 # Fix the printout
 print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
-
+##############################################
+# Output
+#I started with $100 and now have $194.87171000000012. Awesome!
 # Definition of pi_string
 pi_string = "3.1415926"
 
