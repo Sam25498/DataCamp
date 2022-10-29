be73fb8866ea5fad9990032095a089cea98266aa
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-10-29 22:37:57 +0300

    Update Subsetting Python lists.py

--- Python/Introduction to Python/Chapter 2/Subsetting Python lists.py
+++ Python/Introduction to Python/Chapter 2/Subsetting Python lists.py
@@ -4,3 +4,8 @@ areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.7
 # Print out second element from areas
 print(areas[1])
 
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
