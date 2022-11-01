73a93a69915203ff99a75a4555f183ff4b1612f8
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-01 22:14:51 +0300

    Update Deleting elements.py

--- Python/Introduction to Python/Chapter 2/Deleting elements.py
+++ Python/Introduction to Python/Chapter 2/Deleting elements.py
@@ -9,3 +9,7 @@ In [3]:
 print(areas)
 ['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 24.5, 15.45]
 In [4]:
+del(areas[-4:-2])
+In [5]:
+print(areas)
+['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 24.5, 15.45]
