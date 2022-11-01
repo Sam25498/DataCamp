d000aafc64b0cfd1d98fb6a5a3e2c395dc4449a3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-01 22:04:17 +0300

    Update Extend a list.py

--- Python/Introduction to Python/Chapter 2/Extend a list.py
+++ Python/Introduction to Python/Chapter 2/Extend a list.py
@@ -10,3 +10,7 @@ areas_2 = areas_1 + ["garage", 15.45]
 
 print(areas_1)
 print(areas_2)
+
+#output:
+    ['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 'poolhouse', 24.5]
+    ['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 'poolhouse', 24.5, 'garage', 15.45]
