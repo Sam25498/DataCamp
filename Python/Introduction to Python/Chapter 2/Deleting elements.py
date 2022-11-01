1a20c3a6895f3a5755e6e80f9a615bcc96104743
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-01 22:14:32 +0300

    Create Deleting elements.py

---
+++ Python/Introduction to Python/Chapter 2/Deleting elements.py
@@ -0,0 +1,11 @@
+In [1]:
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+In [2]:
+del(areas[10]); del(areas[11])
+In [3]:
+print(areas)
+['hallway', 11.25, 'kitchen', 18.0, 'chill zone', 20.0, 'bedroom', 10.75, 'bathroom', 10.5, 24.5, 15.45]
+In [4]:
