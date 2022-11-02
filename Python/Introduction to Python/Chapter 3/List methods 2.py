11180d2d59def0c9c60f7eb7b741b6c01e30bf74
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-02 23:24:00 +0300

    Update List methods 2.py

--- Python/Introduction to Python/Chapter 3/List methods 2.py
+++ Python/Introduction to Python/Chapter 3/List methods 2.py
@@ -4,3 +4,14 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Use append twice to add poolhouse and garage size
 areas.append(24.5)
 areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+[11.25, 18.0, 20.0, 10.75, 9.5, 24.5, 15.45]
+[15.45, 24.5, 9.5, 10.75, 20.0, 18.0, 11.25]
