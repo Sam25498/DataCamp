31ca9cf34543eb09a6ced141e5b586ff3d4a06de
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-02 23:34:36 +0300

    Update Import packages.py

--- Python/Introduction to Python/Chapter 3/Import packages.py
+++ Python/Introduction to Python/Chapter 3/Import packages.py
@@ -10,3 +10,12 @@ C = 2 * math.pi * r
 # Calculate A
 A = math.pi * r**2
 
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+Circumference: 2.701769682087222
+Area: 0.5808804816487527
+
+<script.py> output:
+    Circumference: 2.701769682087222
+    Area: 0.5808804816487527
