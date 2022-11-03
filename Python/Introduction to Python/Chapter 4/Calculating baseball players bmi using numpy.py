1eb8b244e8be9b99374e83f2752f1978e3eee5c0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-03 23:37:46 +0300

    Update Calculating baseball players bmi using numpy.py

--- Python/Introduction to Python/Chapter 4/Calculating baseball players bmi using numpy.py
+++ Python/Introduction to Python/Chapter 4/Calculating baseball players bmi using numpy.py
@@ -9,3 +9,14 @@ np_height_m = np.array(height_in) * 0.0254
 # Create array from weight_lb with metric units: np_weight_kg
 np_weight_kg = np.array(weight_lb) * 0.453592
 
+# Calculate the BMI: bmi
+bmi = np_weight_kg / np_height_m ** 2
+
+# Print out bmi
+print(bmi)
+[23.11037639 27.60406069 28.48080465 ... 25.62295933 23.74810865
+ 25.72686361]
+
+<script.py> output:
+    [23.11037639 27.60406069 28.48080465 ... 25.62295933 23.74810865
+     25.72686361]
