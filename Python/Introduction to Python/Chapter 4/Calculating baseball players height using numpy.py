e62fb5147ab8215df2c8c45e1a7f5477ce6ce93c
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-03 23:32:39 +0300

    Create Calculating baseball players height using numpy.py

---
+++ Python/Introduction to Python/Chapter 4/Calculating baseball players height using numpy.py
@@ -0,0 +1,11 @@
+# height_in is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
