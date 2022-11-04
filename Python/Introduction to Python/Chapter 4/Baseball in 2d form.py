0334559c23c0d411169ac9d9289c29f168ab31aa
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-04 23:29:57 +0300

    Create Baseball in 2d form.py

---
+++ Python/Introduction to Python/Chapter 4/Baseball in 2d form.py
@@ -0,0 +1,8 @@
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
