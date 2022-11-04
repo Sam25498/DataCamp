bb0177031b3903f86370729555eb093d229b32db
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-04 23:30:08 +0300

    Update Baseball in 2d form.py

--- Python/Introduction to Python/Chapter 4/Baseball in 2d form.py
+++ Python/Introduction to Python/Chapter 4/Baseball in 2d form.py
@@ -6,3 +6,6 @@ import numpy as np
 # Create a 2D numpy array from baseball: np_baseball
 np_baseball = np.array(baseball)
 
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+(1015, 2)
