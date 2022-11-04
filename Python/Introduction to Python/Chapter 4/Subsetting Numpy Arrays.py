c718d8cab9e999b8f122b81c4fa91dcae390ff63
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-04 23:16:05 +0300

    Update Subsetting Numpy Arrays.py

--- Python/Introduction to Python/Chapter 4/Subsetting Numpy Arrays.py
+++ Python/Introduction to Python/Chapter 4/Subsetting Numpy Arrays.py
@@ -12,3 +12,7 @@ print(np_weight_lb[50])
 
 # Print out sub-array of np_height_in: index 100 up to and including index 110
 print(np_height_in[100:111])
+
+#output:
+    200
+    [73 74 72 73 69 72 73 75 75 73 72]
