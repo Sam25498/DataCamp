8fc822c84a10129a184394faadda8a4200c522fd
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-04 23:15:26 +0300

    Create Subsetting Numpy Arrays.py

---
+++ Python/Introduction to Python/Chapter 4/Subsetting Numpy Arrays.py
@@ -0,0 +1,9 @@
+# height_in and weight_lb are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
