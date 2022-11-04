1f6118617b80f42c53d886418e05ea43e90c0c36
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-04 23:01:53 +0300

    Update Numpy side effects.py

--- Python/Introduction to Python/Chapter 4/Numpy side effects.py
+++ Python/Introduction to Python/Chapter 4/Numpy side effects.py
@@ -5,3 +5,13 @@ array([4, 5, 2])
 In [2]:
 np.array([True, 1, 2, 3, 4, False])
 Out[2]:
+
+array([1, 1, 2, 3, 4, 0])
+In [3]:
+np.array([4, 3, 0]) + np.array([0, 2, 2])
+Out[3]:
+array([4, 5, 2])
+In [4]:
+np.array([1, 1, 2]) + np.array([3, 4, -1])
+Out[4]:
+array([4, 5, 1])
