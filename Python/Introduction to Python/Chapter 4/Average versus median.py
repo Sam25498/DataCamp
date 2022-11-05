c3ed96eea4480843513524b3a29daaa60b43c29f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-05 21:10:02 +0300

    Update Average versus median.py

--- Python/Introduction to Python/Chapter 4/Average versus median.py
+++ Python/Introduction to Python/Chapter 4/Average versus median.py
@@ -7,3 +7,7 @@ import numpy as np
 np_height_in = np_baseball[:,0]
 
 # Print out the mean of np_height_in
+print(np.mean(np_height_in))
+
+# Print out the median of np_height_in
+print(np.median(np_height_in))
