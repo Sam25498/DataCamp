c5dfe4e8158702cc148cc64b2fd00fdb29ec31e8
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-05 20:58:59 +0300

    Create 2D Arithmetic.py

---
+++ Python/Introduction to Python/Chapter 4/2D Arithmetic.py
@@ -0,0 +1,8 @@
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
