18a957f30646bf1271442a773d42ad84c6ce0204
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-05 20:59:20 +0300

    Update 2D Arithmetic.py

--- Python/Introduction to Python/Chapter 4/2D Arithmetic.py
+++ Python/Introduction to Python/Chapter 4/2D Arithmetic.py
@@ -6,3 +6,10 @@ import numpy as np
 
 # Create np_baseball (3 cols)
 np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
