0f873154e24ffc2b90981ae330b92e70811adcd6
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-05 21:16:03 +0300

    Create Exploring summary statistics with numpy.py

---
+++ Python/Introduction to Python/Chapter 4/Exploring summary statistics with numpy.py
@@ -0,0 +1,13 @@
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:, 0])
+print("Median: " + str(med))
+
