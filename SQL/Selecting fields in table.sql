9d7bdc89c81f14d69c3ae434109facae340b2c57
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-23 22:39:58 +0300

    Update Selecting fields in table.sql

--- SQL/Selecting fields in table.sql
+++ SQL/Selecting fields in table.sql
@@ -7,3 +7,5 @@ id	title	author	year	genre
 2	11/22/63: A Novel	Stephen King	2011	Fiction
 3	12 Rules for Life: An Antidote to Chaos	Jordan B. Peterson	2018	Non Fiction
 4	1984 (Signet Classics)	George Orwell	2017	Fiction
+5	5,000 Awesome Facts (About Everything!) (National Geographic Kids)	National Geographic Kids	2019	Non Fiction
+6	A Dance with Dragons (A Song of Ice and Fire)	George R. R. Martin	2011	Fiction
