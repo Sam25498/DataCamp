e45b6066d6d70c8119402c07b99299cf274ecd96
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-23 22:39:28 +0300

    Update Selecting fields in table.sql

--- SQL/Selecting fields in table.sql
+++ SQL/Selecting fields in table.sql
@@ -1,2 +1,7 @@
 -- Return all titles from the books table
 SELECT * FROM books;
+
+#OUTPUT
+id	title	author	year	genre
+1	10-Day Green Smoothie Cleanse	JJ Smith	2016	Non Fiction
+2	11/22/63: A Novel	Stephen King	2011	Fiction
