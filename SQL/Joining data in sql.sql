860ab9518bf2a0cfd88114e65a587a02903db266
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-26 14:47:08 +0300

    Update Joining data in sql.sql

--- SQL/Joining data in sql.sql
+++ SQL/Joining data in sql.sql
@@ -7,3 +7,10 @@ name	country_code	city_proper_pop	metroarea_pop	urbanarea_pop
 Abidjan	CIV	4765000	null	4765000
 Abu Dhabi	ARE	1145000	null	1145000
 ...,
+
+SELECT * 
+FROM cities
+-- Inner join to countries
+INNER JOIN cities
+-- Match on country codes
+ON countries.code = cities.country_code;
