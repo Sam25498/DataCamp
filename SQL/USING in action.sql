223e88f4ee873a42c624225455dddc9f5282872c
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-26 23:06:55 +0300

    Update USING in action.sql

--- SQL/USING in action.sql
+++ SQL/USING in action.sql
@@ -1,3 +1,4 @@
+-- Use the country code field to complete the INNER JOIN with USING; do not change any alias names.
 SELECT c.name AS country, l.name AS language, official
 FROM countries AS c
 INNER JOIN languages AS l
