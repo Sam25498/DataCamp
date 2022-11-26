d3e0226a595b728c4df23c1c9056418e0c14bf85
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-26 22:27:24 +0300

    Update Aliasing and Joining Data in Sql.sql

--- SQL/Aliasing and Joining Data in Sql.sql
+++ SQL/Aliasing and Joining Data in Sql.sql
@@ -3,3 +3,12 @@ SELECT cities.name AS city , countries.name AS country , countries.region AS reg
 FROM cities 
 INNER JOIN countries 
 ON cities.country_code = countries.code;
+
+--query result
+countries cities
+
+city	country	region
+Abidjan	Cote d'Ivoire	Western Africa
+Abu Dhabi	United Arab Emirates	Middle East
+Abuja	Nigeria	Western Africa
+Accra	Ghana	Western Africa
