f612a66b93b868aaa53a99669be29a43b6f9f2bb
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-26 23:35:40 +0300

    Update Inspecting a relationship btw fields.sql

--- SQL/Inspecting a relationship btw fields.sql
+++ SQL/Inspecting a relationship btw fields.sql
@@ -17,3 +17,38 @@ Afghanistan	Other
 Albania	Albanian
 Albania	Greek
 Albania	Other
+
+--Rearrange the SELECT statement so that the language column appears on the left and the country column on the right.
+-- Sort the results by language.
+-- Rearrange SELECT statement, keeping aliases
+SELECT l.name AS language, c.name AS country
+FROM countries AS c
+INNER JOIN languages AS l
+USING(code)
+-- Order the results by language
+ORDER BY language;
+
+-- query result
+
+language	country
+Afar	Ethiopia
+Afar	Eritrea
+Afar	Djibouti
+Afrikaans	Namibia
+Afrikaans	South Africa
+Akyem	Ghana
+
+SELECT c.name AS country, l.name AS language
+FROM countries AS c
+INNER JOIN languages AS l
+USING(code)
+ORDER BY country;
+
+-- query result
+country	language
+Afghanistan	Pashto
+Afghanistan	Dari
+Afghanistan	Turkic
+Afghanistan	Other
+Albania	Other
+Albania	Greek
