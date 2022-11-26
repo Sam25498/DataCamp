ef29bfe6b0e1f315884ce0779ca25678eb4b4712
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-26 14:46:38 +0300

    Update Joining data in sql.sql

--- SQL/Joining data in sql.sql
+++ SQL/Joining data in sql.sql
@@ -1,3 +1,9 @@
 -- Select all columns from cities
 SELECT * 
 FROM cities;
+
+--query result
+name	country_code	city_proper_pop	metroarea_pop	urbanarea_pop
+Abidjan	CIV	4765000	null	4765000
+Abu Dhabi	ARE	1145000	null	1145000
+...,
