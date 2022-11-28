f63d8590ec5becc6db1650691f24079bfd8339ca
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 14:16:00 +0300

    Update LEFT JOIN and INNER JOIN in action.sql

--- SQL/LEFT JOIN and INNER JOIN in action.sql
+++ SQL/LEFT JOIN and INNER JOIN in action.sql
@@ -9,3 +9,11 @@ FROM cities AS c1
 INNER JOIN countries as c2
 ON c1.country_code = c2.code
 ORDER BY code DESC;
+
+--query result
+--city	code	country	region	city_proper_pop
+Harare	ZWE	Zimbabwe	Eastern Africa	1606000
+Lusaka	ZMB	Zambia	Eastern Africa	1742979
+Cape Town	ZAF	South Africa	Southern Africa	3740026
+Johannesburg	ZAF	South Africa	Southern Africa	4434827
+Durban	ZAF	South Africa	Southern Africa	3442361
