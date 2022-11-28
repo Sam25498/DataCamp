239d0fa068c69c06a62fbd85b212c40b90c654b5
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 13:52:33 +0300

    Update Checking multi-table joins.sql

--- SQL/Checking multi-table joins.sql
+++ SQL/Checking multi-table joins.sql
@@ -6,3 +6,11 @@ INNER JOIN economies AS e
 ON c.code = e.code
 -- Add an additional joining condition such that you are also joining on year
 	AND e.year = p.year;
+	
+--query result
+Angola	2015	5.996	null
+Albania	2010	1.663	14
+Albania	2015	1.793	17.1
+United Arab Emirates	2010	1.868	null
+United Arab Emirates	2015	1.767	null
+Argentina	2010	2.37	7.75
