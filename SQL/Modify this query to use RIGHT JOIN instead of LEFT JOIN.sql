f6fe718f026501ae88f12acc1fe54345985c4adb
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 19:07:35 +0300

    Update Modify this query to use RIGHT JOIN instead of LEFT JOIN.sql

--- SQL/Modify this query to use RIGHT JOIN instead of LEFT JOIN.sql
+++ SQL/Modify this query to use RIGHT JOIN instead of LEFT JOIN.sql
@@ -11,3 +11,18 @@ Ethiopia	Afar	1.7
 Djibouti	Afar	null
 Eritrea	Afar	null
 Namibia	Afrikaans	10.4
+
+-- Modified code using RIGHT JOIN instead of LEFT JOIN
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+USING(code)
+ORDER BY language;
+
+--query result
+
+country	language	percent
+Ethiopia	Afar	1.7
+Djibouti	Afar	null
+Eritrea	Afar	null
+Namibia	Afrikaans	10.4
