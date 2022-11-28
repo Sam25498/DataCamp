1991337cf8fab31cfa80e8c2e4677d33a2264464
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 14:18:59 +0300

    Update LEFT JOIN and INNER JOIN in action.sql

--- SQL/LEFT JOIN and INNER JOIN in action.sql
+++ SQL/LEFT JOIN and INNER JOIN in action.sql
@@ -1,3 +1,5 @@
+--Perform an inner join with cities AS c1 on the left and countries as c2 on the right.
+--Use code as the field to merge your tables on.
 SELECT 
     c1.name AS city,
     code,
@@ -17,3 +19,17 @@ Lusaka	ZMB	Zambia	Eastern Africa	1742979
 Cape Town	ZAF	South Africa	Southern Africa	3740026
 Johannesburg	ZAF	South Africa	Southern Africa	4434827
 Durban	ZAF	South Africa	Southern Africa	3442361
+
+--Change the code to perform a LEFT JOIN instead of an INNER JOIN.
+--After executing this query, have a look at how many records the query result contains.
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+-- Join right table (with alias)
+LEFT JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
