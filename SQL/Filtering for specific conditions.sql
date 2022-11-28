35606e1871aa9348b1208355ac726ba562b8347a
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 18:34:16 +0300

    Update Filtering for specific conditions.sql

--- SQL/Filtering for specific conditions.sql
+++ SQL/Filtering for specific conditions.sql
@@ -5,3 +5,11 @@ LEFT JOIN economies AS e
 ON c.code = e.code
 -- Filter for the year 2010
     WHERE year = 2010;
+
+--query result
+
+--name	region	gdp_percapita
+Afghanistan	Southern and Central Asia	539.667
+Angola	Central Africa	3599.27
+Albania	Southern Europe	4098.13
+...,
