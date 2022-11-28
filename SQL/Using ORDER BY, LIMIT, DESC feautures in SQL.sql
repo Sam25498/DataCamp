bd4d44f212162405e86f692e454b7769769d20ab
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 18:56:06 +0300

    Create Using ORDER BY, LIMIT, DESC feautures in SQL.sql

---
+++ SQL/Using ORDER BY, LIMIT, DESC feautures in SQL.sql
@@ -0,0 +1,10 @@
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+GROUP BY region
+-- Order by descending avg_gdp
+ORDER BY avg_gdp DESC
+-- Return only first 10 records
+LIMIT 10;
