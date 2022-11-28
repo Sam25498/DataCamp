9108c6a0d86d03029f722f144362c6ff0882fdd0
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 13:34:03 +0300

    Update Joining Multiple tables.sql

--- SQL/Joining Multiple tables.sql
+++ SQL/Joining Multiple tables.sql
@@ -4,3 +4,11 @@ FROM populations AS p
 -- Inner join countries and populations, aliased, on code
 INNER JOIN countries AS c
 ON p.country_code = c.code;
+
+--query result
+name	year	fertility_rate
+Aruba	2010	1.704
+Aruba	2015	1.647
+Afghanistan	2010	5.746
+Afghanistan	2015	4.653
+Angola	2010	6.41
