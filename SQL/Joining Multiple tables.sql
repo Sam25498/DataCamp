df2cafd233f6f9c69f0855d929459c533ad339c1
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 13:35:53 +0300

    Update Joining Multiple tables.sql

--- SQL/Joining Multiple tables.sql
+++ SQL/Joining Multiple tables.sql
@@ -1,3 +1,6 @@
+-- Perform an inner join of countries AS c (left) with populations AS p (right), on code.
+--Select name, year and fertility_rate.
+
 -- Select relevant fields
 SELECT name, year, fertility_rate
 FROM populations AS p
@@ -12,3 +15,6 @@ Aruba	2015	1.647
 Afghanistan	2010	5.746
 Afghanistan	2015	4.653
 Angola	2010	6.41
+
+--Chain another inner join to your query with the economies table AS e, using code.
+-- Select name, and using table aliases, select year and unemployment_rate from economies.
