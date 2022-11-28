c2bdfdc7d09caa0aebd3ed4cd36d0af98b32176b
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-28 18:44:05 +0300

    Update Using GROUP BY  and AVG funcitons in SQL .sql

--- SQL/Using GROUP BY  and AVG funcitons in SQL .sql
+++ SQL/Using GROUP BY  and AVG funcitons in SQL .sql
@@ -6,3 +6,10 @@ USING(code)
 WHERE year = 2010
 -- Group by region
 GROUP BY region;
+
+--query result
+--region	avg_gdp
+Southern Africa	5051.597973632813
+Caribbean	11413.339454064002
+Eastern Africa	1757.3481616973877
+Southern Europe	22926.410910866478
