b451b7fb21577b2419bc2afe2a917ddfbabd662a
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-29 13:14:34 +0300

    Update and rename Comparing joins FULL JOINS.sql to Comparing joins FULL JOINS vs LEFT JOIN.sql

--- SQL/Comparing joins FULL JOINS vs LEFT JOIN.sql
+++ SQL/Comparing joins FULL JOINS vs LEFT JOIN.sql
@@ -1,3 +1,5 @@
+--Perform a full join with countries (left) and currencies (right).
+--Filter for the North America region or NULL country names.
 SELECT name AS country, code, region, basic_unit
 FROM countries
 -- Join to currencies
@@ -17,3 +19,15 @@ Greenland	GRL	North America	null
 null	TMP	null	United States dollar
 null	FLK	null	Falkland Islands pound
 null	HKG	null	Hong Kong dollar
+
+--Repeat the same query as before, turning your full join into a left join with the currencies table.
+--Have a look at what has changed in the output by comparing it to the full join result.
+
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+LEFT JOIN currencies
+USING (code)
+WHERE region = 'North America' 
+	OR name IS NULL
+ORDER BY region;
