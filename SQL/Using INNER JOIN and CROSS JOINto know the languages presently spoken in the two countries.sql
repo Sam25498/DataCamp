a2b541207f34454506278fc8801a995dd4d4a013
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-29 13:56:09 +0300

    Update and rename Using INNER JOIN to know the languages presently spoken in the two countries.sql to Using INNER JOIN and CROSS JOINto know the languages presently spoken in the two countries.sql

--- SQL/Using INNER JOIN and CROSS JOINto know the languages presently spoken in the two countries.sql
+++ SQL/Using INNER JOIN and CROSS JOINto know the languages presently spoken in the two countries.sql
@@ -22,3 +22,13 @@ Pakistan	English
 Pakistan	Burushaski
 Pakistan	Other
 Showing 25 out of 25 rows
+
+--Change your INNER JOIN to a different kind of join to look at possible combinations of languages that could have been spoken in the two countries given their history.
+--Observe the differences in output for both joins.
+
+SELECT c.name AS country, l.name AS language
+FROM countries AS c        
+-- Perform a cross join to languages (alias as l)
+CROSS JOIN languages AS l
+WHERE c.code in ('PAK','IND')
+	AND l.code in ('PAK','IND');
