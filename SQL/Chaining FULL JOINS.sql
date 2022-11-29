4e4551bbde1a81b91828cd446ae5ae9e7904c3b6
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-29 13:39:47 +0300

    Update Chaining FULL JOINS.sql

--- SQL/Chaining FULL JOINS.sql
+++ SQL/Chaining FULL JOINS.sql
@@ -12,3 +12,13 @@ USING(code)
 FULL JOIN currencies AS c2
 USING(code)
 WHERE region LIKE 'M%esia';
+
+
+--query result
+--country	region	language	basic_unit	frac_unit
+Kiribati	Micronesia	English	Australian dollar	Cent
+Kiribati	Micronesia	Kiribati	Australian dollar	Cent
+Marshall Islands	Micronesia	Other	United States dollar	Cent
+Marshall Islands	Micronesia	Marshallese	United States dollar	Cent
+Nauru	Micronesia	Other	Australian dollar	Cent
+Nauru	Micronesia	English	Australian dollar	Cent
