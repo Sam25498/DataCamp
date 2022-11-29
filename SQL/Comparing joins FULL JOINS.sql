66d68049f697c94db46150b1f57c212c0f413290
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-29 13:10:34 +0300

    Create Comparing joins FULL JOINS.sql

---
+++ SQL/Comparing joins FULL JOINS.sql
@@ -0,0 +1,9 @@
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+FULL JOIN currencies
+USING (code)
+-- Where region is North America or null
+WHERE region = 'North America'
+	OR name IS NULL
+ORDER BY region; 
