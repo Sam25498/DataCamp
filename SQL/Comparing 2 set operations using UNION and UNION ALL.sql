fa76acda7238f61c3911640b34ee51d403c6472a
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-30 17:03:05 +0300

    Update Comparing 2 set operations using UNION and UNION ALL.sql

--- SQL/Comparing 2 set operations using UNION and UNION ALL.sql
+++ SQL/Comparing 2 set operations using UNION and UNION ALL.sql
@@ -5,3 +5,16 @@ UNION
 SELECT country_code, year
 FROM populations
 ORDER BY code, year;
+
+--query result
+economies
+populations
+
+code	year
+ABW	2010
+ABW	2015
+AFG	2010
+AFG	2015
+AGO	2010
+AGO	2015
+ALB	2010
