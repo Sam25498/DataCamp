a32bbbdbdaba0a2779d4aaf39813e34fe8b2e645
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-30 16:55:53 +0300

    Update Using UNION and UNION ALL to compare economies.sql

--- SQL/Using UNION and UNION ALL to compare economies.sql
+++ SQL/Using UNION and UNION ALL to compare economies.sql
@@ -7,3 +7,14 @@ UNION
 SELECT *
 FROM economies2019
 ORDER BY code, year;
+
+--query result
+economies2019
+economies2015
+
+code	year	income_group	gross_savings
+ABW	2015	High income	14.867851
+AGO	2015	Lower middle income	25.021326
+AGO	2019	Lower middle income	25.524847
+ALB	2015	Upper middle income	16.863981
+ALB	2019	Upper middle income	14.499825
