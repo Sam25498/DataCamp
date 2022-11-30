9510f0d705e188cba8735ede8e2b05d982d4c835
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-30 17:05:57 +0300

    Update Comparing 2 set operations using UNION and UNION ALL.sql

--- SQL/Comparing 2 set operations using UNION and UNION ALL.sql
+++ SQL/Comparing 2 set operations using UNION and UNION ALL.sql
@@ -1,3 +1,6 @@
+--Perform an appropriate set operation that determines all pairs of country code and year (in that order) from economies and populations, excluding duplicates.
+--Order by country code and year.
+
 -- Query that determines all pairs of code and year from economies and populations, without duplicates
 SELECT code, year
 FROM economies
@@ -26,3 +29,13 @@ CYP	2015
 CZE	2010
 CZE	2015
 Showing 100 out of 434 rows
+
+--Amend the query to return all combinations (including duplicates) of country code and year in the economies or the populations tables.
+-- Using UNION ALL
+SELECT code, year
+FROM economies
+-- Set theory clause
+UNION ALL
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
