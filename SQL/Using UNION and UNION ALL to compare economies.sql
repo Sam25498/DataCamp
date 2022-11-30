f34441d5d1be5495c72c45a0fb3515558f665f2e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-30 16:56:22 +0300

    Update Using UNION and UNION ALL to compare economies.sql

--- SQL/Using UNION and UNION ALL to compare economies.sql
+++ SQL/Using UNION and UNION ALL to compare economies.sql
@@ -18,3 +18,11 @@ AGO	2015	Lower middle income	25.021326
 AGO	2019	Lower middle income	25.524847
 ALB	2015	Upper middle income	16.863981
 ALB	2019	Upper middle income	14.499825
+...
+ESP	2019	High income	22.992832
+EST	2015	High income	27.578293
+EST	2019	High income	29.931715
+ETH	2015	Low income	29.522158
+ETH	2019	Low income	29.03045
+FIN	2015	High income	20.891329
+Showing 100 out of 317 rows
