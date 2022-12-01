251b0c8d3fbcab8f4da1874c4d284e4f7f7cf5b7
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-01 15:03:16 +0300

    Update Using EXCEPT to return all cities that dont have same name as country.sql

--- SQL/Chapter 3/Using EXCEPT to return all cities that dont have same name as country.sql
+++ SQL/Chapter 3/Using EXCEPT to return all cities that dont have same name as country.sql
@@ -5,3 +5,22 @@ EXCEPT
 SELECT name
 FROM countries
 ORDER BY name;
+
+-- query result
+cities
+countries
+
+name
+Abidjan
+Abu Dhabi
+Abuja
+Accra
+Addis Ababa
+Ahmedabad
+...
+Izmir
+Jaipur
+Jakarta
+Jeddah
+Jinan
+Showing 100 out of 235 rows
