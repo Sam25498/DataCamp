eb760412793fec9dc765953f2a6eb449fe7460db
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-01 15:36:21 +0300

    Update Using Semi joins to filter out middle east spoken languages.sql

--- SQL/Chapter 4/Using Semi joins to filter out middle east spoken languages.sql
+++ SQL/Chapter 4/Using Semi joins to filter out middle east spoken languages.sql
@@ -1,11 +1,8 @@
 -- Select country code for countries in the Middle East
+-- Select country code for countries in the Middle East
 SELECT code
 FROM countries
-WHERE region in
-(SELECT region
-FROM countries
-WHERE region = 'Middle East'
-);
+WHERE region = 'Middle East';
 
 --query result
 languages
