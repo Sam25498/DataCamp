b47e6b6134457e37e31f4c7db8dc832aad37163f
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-02 00:50:10 +0300

    Update Diagnosing Problems using anti join.sql

--- SQL/Chapter 4/Diagnosing Problems using anti join.sql
+++ SQL/Chapter 4/Diagnosing Problems using anti join.sql
@@ -1,4 +1,15 @@
+--Begin your anti join by returning the code and name (in order, not aliased) for all countries in the continent of Oceania from the countries table.v
 -- Select code and name of countries from Oceania
 SELECT code, name
 FROM countries
 WHERE continent LIKE 'Oceania';
+
+--query result
+currencies
+countries
+
+code	name
+ASM	American Samoa
+AUS	Australia
+FJI	Fiji Islands
+GUM	Guam
