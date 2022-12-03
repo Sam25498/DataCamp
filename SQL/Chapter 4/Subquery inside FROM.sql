ee1b535e64438b8ed29dd232dd0ebbe780f7a3dd
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-03 12:22:10 +0300

    Update Subquery inside FROM.sql

--- SQL/Chapter 4/Subquery inside FROM.sql
+++ SQL/Chapter 4/Subquery inside FROM.sql
@@ -58,6 +58,18 @@ Showing 100 out of 212 rows
 ---Select local_name from countries, with the aliased lang_num from your subquery (which has been nested and aliased for you as sub).
 --Use WHERE to match the code field from countries and sub.
 -- Select local_name and lang_num from appropriate tables
+--first try
+-- Select local_name and lang_num from appropriate tables
+SELECT local_name FROM countries
+FROM languages,
+  (SELECT code, COUNT(*) AS lang_num
+  FROM languages
+  GROUP BY code) AS sub
+-- Where codes match
+WHERE countries.code = sub.code
+ORDER BY lang_num DESC;
+
+
 --correctt soln'
 SELECT local_name, sub.lang_num
 FROM countries,
