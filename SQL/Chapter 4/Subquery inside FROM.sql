12b1f2c04dbda9315f0a4bdde7e2bca2a8f417c7
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-03 12:08:44 +0300

    Update Subquery inside FROM.sql

--- SQL/Chapter 4/Subquery inside FROM.sql
+++ SQL/Chapter 4/Subquery inside FROM.sql
@@ -20,3 +20,19 @@ FROM languages,
 SELECT code, COUNT(*) AS lang_num
 FROM languages
 GROUP BY code;
+
+---query result
+languages
+countries
+
+code	lang_num
+PRY	2
+NRU	3
+MDG	3
+ASM	5
+...
+KHM	2
+NPL	14
+IND	14
+EGY	2
+Showing 100 out of 212 rows
