7f67c6442a31a7c8a4e8f01f45fd745720460fec
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-05 16:44:26 +0300

    Update Final Challenge.sql

--- SQL/Chapter 4/Final Challenge.sql
+++ SQL/Chapter 4/Final Challenge.sql
@@ -1,3 +1,20 @@
+-- 1st Attempt
+-- Select fields from cities
+SELECT name, country_code, city_proper_pop, metroarea_pop,city_proper_pop / metroarea_pop * 100 AS city_perc
+FROM cities
+-- Use subquery to filter city name
+WHERE capital IN
+    (SELECT capital
+    FROM countries
+    WHERE region LIKE 'Europe'
+    OR region LIKE '%America')
+-- Add filter condition such that metroarea_pop does not have null values
+AND WHERE metroarea_pop IS NOT NULL
+
+-- Sort and limit the result
+ORDER BY city_perc DESC
+LIMIT 10;
+
 -- 2nd Attempt
 -- Select fields from cities
 SELECT name, country_code, city_proper_pop, metroarea_pop,city_proper_pop / metroarea_pop * 100 AS city_perc
