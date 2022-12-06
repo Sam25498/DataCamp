708f30b4c254f5a4747ba41564450b86b2f335f4
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-06 14:37:02 +0300

    Update Subquery inside SELECT 1.sql

--- SQL/Chapter 4/Subquery inside SELECT 1.sql
+++ SQL/Chapter 4/Subquery inside SELECT 1.sql
@@ -23,3 +23,14 @@ Indonesia	7
 Russian Federation	7
 South Korea	7
 Showing 9 out of 9 rows
+
+-- Write a nested subquery that returns an equivalent result to your LEFT JOIN, finding the counts of cities appearing in the cities table as cities_num
+
+SELECT countries.name AS country,
+-- Subquery that provides the count of cities   
+  (SELECT COUNT(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
