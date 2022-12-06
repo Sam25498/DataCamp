4f7b88843edd77a0245652f6277f6a820e119221
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-06 14:33:43 +0300

    Create Subquery inside SELECT 1.sql

---
+++ SQL/Chapter 4/Subquery inside SELECT 1.sql
@@ -0,0 +1,9 @@
+-- Find top nine countries with the most cities
+SELECT countries.name AS country, COUNT(*) AS cities_num
+FROM countries
+LEFT JOIN cities
+ON countries.code = cities.country_code
+GROUP BY country
+-- Order by count of cities as cities_num
+ORDER BY cities_num DESC, country
+LIMIT 9;
