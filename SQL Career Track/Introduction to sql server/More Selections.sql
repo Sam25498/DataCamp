e7212396d18e7c76a900d0ad70bc5718e5783b84
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-06-19 12:44:08 +0300

    Create More Selections.sql

---
+++ SQL Career Track/Introduction to sql server/More Selections.sql
@@ -0,0 +1,26 @@
+--SELECT the country and event_year columns from the eurovision table.
+-- Amend the code to select all columns
+SELECT 
+  country, 
+  event_year 
+FROM 
+  eurovision;
+ ########################
+ query result
+eurovision
+country	event_year
+Israel	2009
+France	2009
+Sweden	2009
+Croatia	2009
+Portugal	2009
+Iceland	2009
+Greece	2009
+.....
+Sweden	2006
+Turkey	2006
+Armenia	2006
+Hungary	2005
+United Kingdom	2005
+Malta	2005
+Showing 100 out of 647 rows
