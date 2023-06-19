fa9db4564b1392ac3784f7c3148f8f72a5364afb
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-06-19 13:05:27 +0300

    Update Order by.sql

--- SQL Career Track/Introduction to sql server/Order by.sql
+++ SQL Career Track/Introduction to sql server/Order by.sql
@@ -19,3 +19,29 @@ Winter Storm	2011-01-12
 Firm System Load Shed	2011-01-13
 Vandalism	2011-01-18
 Vandalism	2011-01-23
+
+---------------------------------------------------
+-- Select the top 20 rows from description, nerc_region and event_date
+SELECT 
+  TOP (20) description,
+  nerc_region,
+  event_date
+FROM 
+  grid 
+  -- Order by nerc_region, affected_customers & event_date
+  -- Event_date should be in descending order
+ORDER BY
+  nerc_region,
+  affected_customers,
+  event_date DESC;
+  
+  -------------------------------------------------
+  query result
+grid
+description	nerc_region	event_date
+Suspected Physical Attack	ERCOT	2014-06-12
+Fuel Supply Emergency  Coal	ERCOT	2014-06-06
+Physical Attack  Vandalism	ERCOT	2014-06-03
+Suspected Physical Attack	FRCC	2013-03-18
+Load Shed of 100+ MW Under Emergency Operational Policy	FRCC	2013-06-17
+Firm System Load Shed	FRCC	2011-01-13
