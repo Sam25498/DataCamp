2a5d958ae6b7c78acbdfeb7729d3000f4996c7b6
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-06-21 12:42:01 +0300

    Create LEN'gth of a string.sql

---
+++ SQL Career Track/Introduction to sql server/LEN'gth of a string.sql
@@ -0,0 +1,23 @@
+-- Retrieve the length of the description column, returning the results as description_length.
+-- Calculate the length of the description column
+SELECT 
+  LEN (description) AS description_length
+FROM 
+  grid;
+-----------------------------------------------
+query result
+grid
+description_length
+29
+29
+27
+26
+26
+26
+...
+41
+41
+26
+26
+25
+Showing 100 out of 806 rows
