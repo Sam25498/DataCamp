dbce114e1aff09c78fdde5ae3403ec40ec807bd1
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-06-21 12:22:38 +0300

    Update COUNT in sql.sql

--- SQL Career Track/Introduction to sql server/COUNT in sql.sql
+++ SQL Career Track/Introduction to sql server/COUNT in sql.sql
@@ -8,3 +8,20 @@ FROM
 grid
 grid_total
 806
+------------------------------------------------------------------------
+-- Make the count more meaningful by restricting it to records where the nerc_region is 'RFC'. Name the result RFC_count.
+
+-- Obtain a count of 'grid_id'
+SELECT 
+  COUNT(grid_id) AS grid_total
+FROM 
+  grid
+-- Restrict to rows where the nerc_region is 'RFC'
+WHERE
+  nerc_region = 'RFC';
+-----------------------------------------------------------------------
+
+query result
+grid
+grid_total
+246
