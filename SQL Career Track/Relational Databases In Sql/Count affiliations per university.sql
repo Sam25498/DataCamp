9ecc96a3564eb25c7014359dc3d140d09d49dc6b
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-03 00:55:07 +0300

    Create Count affiliations per university.sql

---
+++ SQL Career Track/Relational Databases In Sql/Count affiliations per university.sql
@@ -0,0 +1,23 @@
+-- Count the total number of affiliations per university
+SELECT COUNT(*), professors.university_id 
+FROM professors
+JOIN affiliations
+ON affiliations.professor_id = professors.id
+-- Group by the university ids of professors
+GROUP BY professors.university_id
+ORDER BY count DESC;
+
+----------------------------------------------
+--query result
+count	university_id
+579	EPF
+273	USG
+162	UBE
+133	ETH
+75	UBA
+40	UFR
+36	UNE
+35	ULA
+33	UGE
+7	UZH
+4	USI
