09b6cfeac4071f5f3ab153eb1b228875356fee14
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-02 23:49:23 +0300

    Create Explore foreign key constraints.sql

---
+++ SQL Career Track/Relational Databases In Sql/Explore foreign key constraints.sql
@@ -0,0 +1,5 @@
+--Run the sample code and have a look at the error message.
+--What's wrong? Correct the university_id so that it actually reflects where Albert Einstein wrote his dissertation and became a professor – at the University of Zurich (UZH)!
+-- Try to insert a new professor
+INSERT INTO professors (firstname, lastname, university_id)
+VALUES ('Albert', 'Einstein', 'UZH');
