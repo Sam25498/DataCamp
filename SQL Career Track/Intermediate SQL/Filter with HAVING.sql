7760b398f2b965eb0eef7fa2358dd72f6149e7c1
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-04 00:49:18 +0300

    Create Filter with HAVING.sql

---
+++ SQL Career Track/Intermediate SQL/Filter with HAVING.sql
@@ -0,0 +1,13 @@
+
+-- Select the country and distinct count of certification as certification_count
+SELECT country, COUNT(DISTINCT certification) AS certification_count
+FROM films
+-- Group by country
+GROUP BY country
+-- Filter results to countries with more than 10 different certifications
+HAVING COUNT(DISTINCT certification) > 10;
+--------------------------------------------------------------------------
+query result
+films
+country	certification_count
+USA	12
