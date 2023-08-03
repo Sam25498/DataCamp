53157f876c8c048ebae774d61a313d9d0b2f88c9
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-04 00:00:26 +0300

    Create Sorting single fields.sql

---
+++ SQL Career Track/Intermediate SQL/Sorting single fields.sql
@@ -0,0 +1,35 @@
+
+-- Select name from people and sort alphabetically
+SELECT name
+FROM people
+ORDER BY name ASC ;
+---------------------------------------------------
+query result
+people
+films
+reviews
+name
+50 Cent
+A. Michael Baldwin
+A. Raven Cruz
+A.J. Buckley
+A.J. DeLucia
+A.J. Lan
+
+--------------------------------------------------------------
+-- Select the title and duration from longest to shortest film
+SELECT title, duration
+FROM films
+ORDER BY duration DESC;
+---------------------------------------------------------------------------
+query result
+people
+films
+reviews
+title	duration
+Destiny	null
+Should've Been Romeo	null
+Hum To Mohabbat Karega	null
+Harry Potter and the Deathly Hallows: Part I	null
+Barfi	null
+Romantic Schemer
