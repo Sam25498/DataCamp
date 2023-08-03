b7ca4053a229085c831d1b863c81755865dcd75c
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-03 22:42:26 +0300

    Create Rounding results.sql

---
+++ SQL Career Track/Intermediate SQL/Rounding results.sql
@@ -0,0 +1,14 @@
+
+-- Round duration_hours to two decimal places
+SELECT title, ROUND((duration / 60.0),2) AS duration_hours
+FROM films;
+-------------------------------------------------------
+
+--query result
+title	duration_hours
+  
+Intolerance: Love's Struggle Throughout the Ages	2.05
+Over the Hill to the Poorhouse	1.83
+The Big Parade	2.52
+Metropolis	2.42
+Pandora's Box	1.83
