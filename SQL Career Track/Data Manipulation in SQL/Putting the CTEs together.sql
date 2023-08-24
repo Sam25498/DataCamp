5b0df72f05a4bd3c2b7f78d384a254249a0fc662
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-08-24 04:19:58 +0300

    Create Putting the CTEs together.sql

---
+++ SQL Career Track/Data Manipulation in SQL/Putting the CTEs together.sql
@@ -0,0 +1,46 @@
+/*Declare the home and away CTEs before your main query.
+Join your CTEs to the match table using a LEFT JOIN.
+Select the relevant data from the CTEs into the main query.
+Select the date from match, team names from the CTEs, and home/ away goals from match in the main query.*/
+-- Set up the home team CTE
+WITH home AS (
+  SELECT m.id, t.team_long_name,
+	  CASE WHEN m.home_goal > m.away_goal THEN 'MU Win'
+		   WHEN m.home_goal < m.away_goal THEN 'MU Loss' 
+  		   ELSE 'Tie' END AS outcome
+  FROM match AS m
+  LEFT JOIN team AS t ON m.hometeam_id = t.team_api_id),
+-- Set up the away team CTE
+away AS (
+  SELECT m.id, t.team_long_name,
+	  CASE WHEN m.home_goal > m.away_goal THEN 'MU Win'
+		   WHEN m.home_goal < m.away_goal THEN 'MU Loss' 
+  		   ELSE 'Tie' END AS outcome
+  FROM match AS m
+  LEFT JOIN team AS t ON m.awayteam_id = t.team_api_id)
+-- Select team names, the date and goals
+SELECT DISTINCT
+    m.date,
+    home.team_long_name AS home_team,
+    away.team_long_name AS away_team,
+    m.home_goal,
+    m.away_goal
+-- Join the CTEs onto the match table
+FROM match AS m
+LEFT JOIN home ON m.id = home.id
+LEFT JOIN away ON m.id = away.id
+WHERE m.season = '2014/2015'
+      AND (home.team_long_name = 'Manchester United' 
+           OR away.team_long_name = 'Manchester United');
+------------------------------------------------------------------------------
+query result
+team
+match
+date	home_team	away_team	home_goal	away_goal
+2014-08-16	Manchester United	Swansea City	1	2
+2014-08-24	Sunderland	Manchester United	1	1
+2014-08-30	Burnley	Manchester United	0	0
+2014-09-14	Manchester United	Queens Park Rangers	4	0
+2014-09-21	Leicester City	Manchester United	5	3
+2014-09-27	Manchester United	West Ham United	2	1
+2014-10-05	Manchester United	Everton	2	1
