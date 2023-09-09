dd7cec237b107fc7c94422a8b64ee78c6ff3b1c0
Byegon <71707603+Sam25498@users.noreply.github.com>
2023-09-09 04:58:18 +0300

    Create Find missing dates.sql

---
+++ SQL Career Track/Exploratory Data Analysis in SQL/Find missing dates.sql
@@ -0,0 +1,13 @@
+SELECT day
+-- 1) Subquery to generate all dates
+-- from min to max date_created
+  FROM (SELECT generate_series(min(date_created),
+                               max(date_created),
+                               1)::interval AS day
+          -- What table is date_created in?
+          FROM evanston311) AS all_dates
+-- 4) Select dates (day from above) that are NOT IN the subquery
+ WHERE day NOT IN day 
+       -- 2) Subquery to select all date_created values as dates
+       (SELECT day::date
+          FROM evanston311);
